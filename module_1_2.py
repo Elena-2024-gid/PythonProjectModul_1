e04a88d65f9048c65bdc3f521da985185b27bdab
Elena-2024-gid <semenenko.elena.vladim.1973@gmail.com>
2024-11-19 13:30:25 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks=12
+number_of_hours_spent=1.5
+course='Python'
+average_execution=number_of_hours_spent/number_of_completed_tasks
+print('Курс',course,'всего задач:', number_of_completed_tasks, 'затрачено часов:', number_of_hours_spent, 'среднее время выполнения:',average_execution, 'часа')
